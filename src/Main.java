626a124fa7008bcb5172d02445d29da24ec64a52
angep73 <p.umunyana@alustudent.com>
2025-03-13 17:50:59 +0200

    adding word java

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Ask for number of rows for the pyramid and triangles
-        System.out.print("Enter the number of rows: ");
-        int rows = scanner.nextInt();
-
-
-
-
-
-        // Print the Right-Angled Star Triangle (Right-aligned)
-        System.out.println("\nRight-Angled Star Triangle (Right-aligned):");
-        for (int i = 1; i <= rows; i++) {
-            // Print leading spaces
-            for (int j = 1; j <= rows - i; j++) {
-                System.out.print(" ");
-            }
-            // Print stars
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        scanner.close();
+        System.out.println("    j     A          v        v      A    ");
+        System.out.println("    j    A  A         v     v       A  A   ");
+        System.out.println("j   j  A A A A A       v  v        A A A A ");
+        System.out.println(" jj  A           A      v         A        A");
     }
 }
